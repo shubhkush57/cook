eccc80bacc80c3fe160aa32af1f45bfb34b50c64
Shubahm <shubham.20205157@mnnit.ac.in>
2024-11-01 13:16:01 +0530

    updae func

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import React, {useState}from 'react'
 
 function App() {
   const [count,setCount] = useState(0);
+  const incremntFun = () =>{
+    setCount(count+1);
+  }
+  const decrementFun = ()=>{
+    setCount(Math.max(0,count-1));
+  }
   return (
     <>
-    <button onClick={()=>setCount(Math.max(0,count-1))}>-</button>
+    <button onClick={decrementFun}>-</button>
     <span>{count}</span>
-    <button onClick={()=>setCount(count+1)}>+</button>
+    <button onClick={incremntFun}>+</button>
     </>
   );
 }
